5ab7bae7d069767cb21bc3b780b171cb9847038f
ma-robles <miguel.robles@atmosfera.unam.mx>
2018-02-14 18:09:38 -0600

    add crop R
    added crop and win right side

--- play_vd.sh
+++ play_vd.sh
@@ -1,3 +1,3 @@
 cd /home/pi/anima
 pkill omxplayer
-omxplayer --loop -b --alpha 198 $(printf anima_%02d.avi $1)
+omxplayer --win 0,480,486,1265 --crop 500,0,986,650 --loop -b --alpha 198 $(printf anima_%02d.avi $1)
