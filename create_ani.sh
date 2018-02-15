dddbe7f5b08ca42f33ce2159fbb8f3dc5ea90dd3
ma-robles <miguel.robles@atmosfera.unam.mx>
2018-02-15 17:02:36 -0600

    increase quality and fixed bugs
    Increase quality in output
    fix bug on video list
    crontab included

--- create_ani.sh
+++ create_ani.sh
@@ -12,10 +12,10 @@ while IFS='' read -u $fd line || [[ -n "$line" ]]; do
         #get all jpg n folder, no cpy tree structure
         wget -nd -r -l 2 -P ./img/ -A jpg -e robots=off $IFOLDER
         cd img/
-        #convert images to video fps=2
+        #convert images to video fps=25
         VNAME=$(printf ../anima_%02d.avi $NVID)
         echo "Generating: " $VNAME
-        ffmpeg -y -framerate 1 -pattern_type glob -i '*.jpg' -r 25  $VNAME 2>ffmpeg.log
+        ffmpeg -y -framerate 1 -pattern_type glob -i '*.jpg' -r 25 -crf 0 $VNAME 2>ffmpeg.log
         let NVID=NVID+1
         #delete images
         echo "Deleting images"
@@ -26,13 +26,3 @@ done
 #close file
 <&$fd- 
 
-#NIMAGES=120
-#NIMAGES=12
-#I=0
-#while [ $NIMAGES -gt $I ]; do
-    #NSTR=$(printf *%03d.* $I)
-    #echo $NSTR
-    #montage -geometry 960x540+5+100 $(find  $(cat filelist.txt) -name "$NSTR"  -type f) $1 auto_mos_"$(printf %03d $I)".jpg
-    #let I=I+1
-#done
-#ffmpeg -y -r 1 -i auto_mos_%03d.jpg anima_all.avi
